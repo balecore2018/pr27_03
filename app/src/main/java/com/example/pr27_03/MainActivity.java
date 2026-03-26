032ec9401aa264cfee0b85017c80c58969ed015b
dima <balecored@gmail.com>
2026-03-26 10:43:00 +0500

    прослушиватель местоположения шаповалов

--- app/src/main/java/com/example/pr27_03/MainActivity.java
+++ app/src/main/java/com/example/pr27_03/MainActivity.java
@@ -1,24 +1,48 @@
 package com.example.pr27_03;
 
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.runtime.image.ImageProvider;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
+
+    LocationListener _locationListnaet = new LocationListener() {
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            if(location != null) {
+                mapView.getMap().move(
+                        new CameraPosition(
+                                new Point(location.getLatitude(), location.getLongitude()), 15, 0, 0));
+                mapView.getMap().getMapObjects.addPlacemark(
+                        new Point(location.getLatitude(), location.getLongitude()),
+                        ImageProvider.fromResource(MainActivity.this, R.drawable.location)
+                );
+                GetAddressByGPS getAddressByGPS = new GetAddressByGPS(
+                        String.valueOf(location.getLongitude()) + "," + String.valueOf(location.getLatitude()),
+                        textAddress
+                );
+
+                getAddressByGPS.execute();
+            }
+        }
+    };
+
 }
+
