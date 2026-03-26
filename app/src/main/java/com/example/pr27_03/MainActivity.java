5d235e9b192e059465beb30aede0facb7f17d1eb
dima <balecored@gmail.com>
2026-03-26 11:37:01 +0500

    онстарт онстоп шаповалов

--- app/src/main/java/com/example/pr27_03/MainActivity.java
+++ app/src/main/java/com/example/pr27_03/MainActivity.java
@@ -1,27 +1,30 @@
 package com.example.pr27_03;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.location.LocationManager;
 import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.Map;
+import com.yandex.mapkit.mapview.MapView;
 import com.yandex.runtime.image.ImageProvider;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
     LocationListener _locationListnaet = new LocationListener() {
         @Override
@@ -43,6 +46,43 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+
+    LocationManager _LocationManager;
+    TextView textAddress;
+    MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MapKitFactory.setApiKey("d129d4b2-4ab3-4fce-bccc-aa45cc87a457");
+        MapKitFactory.initialize(this);
+        setContentView(R.layout.activity_main);
+        mapView = findViewById(R.id.mapview);
+        textAddress = findViewById(R.id.edittext);
+        _LocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        MapKitFactory.getInstance().onStart();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[] { Manifest.permission.ACCESS_FINE_LOCATION }, 1);
+        }
+
+        _LocationManager.requestLocationUpdate(LocationManager.NETWORK_PROVIDER, 1000, 10, _locationListnaet);
+        _LocationManager.requestLocationUpdate(LocationManager.GPS_PROVIDER, 1000, 10, _locationListnaet);
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        mapView.onStop();
+        MapKitFactory.getInstance().onStop();
+    }
 
 }
 
